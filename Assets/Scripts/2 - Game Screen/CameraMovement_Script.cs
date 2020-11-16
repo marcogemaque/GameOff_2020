bed79f7383851ef87453147f5ab696891d475f8a
marcogemaque <marcogemaque@id.uff.br>
2020-11-15 22:08:20 -0300

    Updated camera movement.
    Still another change will be implemented. This is a first step.

---
+++ Assets/Scripts/2 - Game Screen/CameraMovement_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement_Script : MonoBehaviour
+{
+
+    public Vector3 startVec;
+
+    public void Start()
+    {
+        startVec = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-5, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(5, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, 5, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0, -5, 0);
+        }
+    }
+}
